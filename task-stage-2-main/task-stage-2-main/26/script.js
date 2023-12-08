01d9fdbbfe580da26663edb9504dbb51594ba2b0
Pavelsidko <minimalsay@gmail.com>
2023-12-08 10:46:44 +0700

    labs 22 - 36

---
+++ task-stage-2-main/task-stage-2-main/26/script.js
@@ -0,0 +1,4 @@
+const number = new Number(500)
+console.dir(number)
+console.dir(Number.prototype.__proto__)
+console.dir(Number.prototype.__proto__.__proto__)
---
+++ task-stage-2-main/task-stage-2-main/29/name.js
@@ -0,0 +1,9 @@
+const firstName = 'ivan'
+const surName = 'ivanov'
+const middleName = 'ivanovich'
+const birthYear = '2003'
+const institute = 'IKIT'
+const result1 = firstName +' '+  surName +' '+ middleName +' '+ birthYear +' '+ institute
+const result2 = ` ${firstName} ${surName} ${middleName} ${birthYear} ${institute}`
+console.log(result1)
+console.log(result2)
