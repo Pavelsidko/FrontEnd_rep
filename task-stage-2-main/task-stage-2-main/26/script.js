dfc9756408776e3c5aa71d04bbb2de4adcfe6be3
Pavelsidko <minimalsay@gmail.com>
2023-12-29 11:19:14 +0700

    labs 18, 26, 29, 54-55, 59, 62-65

--- task-stage-2-main/task-stage-2-main/26/script.js
+++ task-stage-2-main/task-stage-2-main/26/script.js
@@ -2,3 +2,13 @@ const number = new Number(500)
 console.dir(number)
 console.dir(Number.prototype.__proto__)
 console.dir(Number.prototype.__proto__.__proto__)
+
+const string = new String('500')
+console.dir(string)
+console.dir(String.prototype.__proto__)
+console.dir(String.prototype.__proto__.__proto__)
+
+const boolean = new Boolean(false)
+console.dir(boolean)
+console.dir(Boolean.prototype.__proto__)
+console.dir(Boolean.prototype.__proto__.__proto__)
--- task-stage-2-main/task-stage-2-main/29/name.js
+++ task-stage-2-main/task-stage-2-main/29/name.js
@@ -3,7 +3,10 @@ const surName = 'ivanov'
 const middleName = 'ivanovich'
 const birthYear = '2003'
 const institute = 'IKIT'
-const result1 = firstName +' '+  surName +' '+ middleName +' '+ birthYear +' '+ institute
-const result2 = ` ${firstName} ${surName} ${middleName} ${birthYear} ${institute}`
+const result1 = 'First name: ' + firstName + '\n' + 'Surname: ' + surName + '\n' +
+    'Middle Name: ' + middleName + '\n' + 'Birth Year: ' + birthYear + '\n' + 'Institute: ' + institute
+
+
+const result2 = ` First name: ${firstName} \n Surname: ${surName} \n Middle Name: ${middleName} \n Birth Year: ${birthYear} \n Institute: ${institute}`
 console.log(result1)
 console.log(result2)
